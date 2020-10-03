04b7bcd541a42f2e932147d7b8831576b4f74400
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-10-03 23:37:15 +0900

    2まで完了

--- 2_fill_in_genre_separatedly.py
+++ 2_fill_in_genre_separatedly.py
@@ -2,25 +2,24 @@ import os
 import pandas as pd
 
 
-# undefined を数える
+#18ファイルを分割して行う
+date ="20201003-012640"
 
-time = "20201003-012640"
-old_dirname = os.path.join("data", time)
-new_dirname = os.path.join("data", "genre_2017_filled", time)
-# dirname = os.path.join("data", "genre_2017_filled", "2020_base")
-os.makedirs(new_dirname, exist_ok=True)
+dirname = os.path.join("data", "genre_2017_filled", date)
+os.makedirs(dirname, exist_ok=True)
 
-# d_2020 = pd.read_csv("data/20201003-012640/vgsales_1.csv")
-# d_2020 = pd.read_csv("./vgsales_2020_base.csv")
+from_dirname = os.path.join("data", date)
 d_2017 = pd.read_csv("./vgsales_2017.csv")
 
 pages = 18 + 1
+
 for page in range(1, pages):
+    d_2020 = pd.read_csv("./vgsales_2020_base.csv")
+
+    print("2020のジャンル不明データ数: " + str(((d_2020["Genre"] == "undefined") & (d_2020["Platform"] != "Series")).sum()))
+    # 関数を作成する
+    # 一つの2020に対して突き合わせを行う
     new_genres = []
-
-    filename = "vgsales_" + str(page) + ".csv"
-    d_2020 = pd.read_csv(os.path.join(old_dirname, filename))
-
     for i_2020, row_2020 in d_2020.iterrows():
         # デバッグ用中断
         # if i_2020 >= 10:
@@ -48,10 +47,9 @@ for page in range(1, pages):
         # if name not found in 2017, genre 2020 is undefined
 
     d_2020["New_Genre"] = new_genres
-    new_pathname = os.path.join(new_dirname, filename)
-    d_2020.to_csv(new_pathname, sep=",", encoding='utf-8', index=False)
+    newfilename = os.path.join(dirname, "vgsales_" + str(page) + ".csv")
+    d_2020.to_csv(newfilename, sep=",", encoding='utf-8', index=False)
 
     print("2020のジャンル不明データ数: " + str(((d_2020["New_Genre"] == "undefined") & (d_2020["Platform"] != "Series")).sum()))
 
-# 関数を作成する
-# 一つの2020に対して突き合わせを行う
+# d_2020 = pd.read_csv("data/20201003-012640/vgsales_1.csv")
--- 3_get_unfilled_genre.py
+++ 3_get_unfilled_genre.py
@@ -1,4 +1,60 @@
+import pandas as pd
+import os
+import requests
+from bs4 import BeautifulSoup, element
+from time import sleep
 
 # TODO: ジャンルがundefinedなデータについてジャンルを個別取得する
 # TODO: Seriesデータはどうせ省くので飛ばして良い
 # TODO: 埋める
+
+def _sleep(n):
+    print("過剰リクエスト防止のための" + str(n) + "秒sleep")
+    for i in range(n, 0, -1):
+        print(i)
+        sleep(1)
+
+pages = 18 + 1
+
+time = "20201003-012640"
+new_dirname = os.path.join("data", "genre_2020_filled", time)
+os.makedirs(new_dirname, exist_ok=True)
+
+dirname = os.path.join("data", "genre_2017_filled", time)
+for page in range(1, pages):
+    old_filename = os.path.join(dirname, "vgsales_" + str(page) + ".csv")
+    d_2020 = pd.read_csv(old_filename)
+    genre_2020 = []
+
+    for i_2020, row_2020 in d_2020.iterrows():
+        # Seriesデータとundefinedじゃないデータは見る必要なし
+        if row_2020["Platform"] == "Series" or row_2020["New_Genre"] != "undefined":
+            print(f"{row_2020['Rank']} : {row_2020['Name']} :{row_2020['New_Genre']}")
+            genre_2020.append(row_2020["New_Genre"])
+            continue
+
+        # find genre
+        url = row_2020["URL"]
+        r = requests.get(url).text
+        _sleep(60)
+        soup = BeautifulSoup(r, "html.parser")
+        print(f"{row_2020['Rank']}: {row_2020['Name']}")
+        if soup.find("div", {"id": "gameGenInfoBox"}) == None:
+            print("sub_soupにgameGenInfoBoxを見つけられなかったのでジャンル不明")
+            genre_2020.append("undefined")
+            continue
+        # else
+        h2s = soup.find("div", {"id": "gameGenInfoBox"}).find_all('h2')
+        # make a temporary tag here to search for the one that contains
+        # the word "Genre"
+        temp_tag = element.Tag
+        for h2 in h2s:
+            if h2.string == 'Genre':
+                temp_tag = h2
+        genre_2020.append(temp_tag.next_sibling.string)
+
+
+    d_2020["Genre_2020"] = genre_2020
+    new_filename = os.path.join(new_dirname, "vgsales_" + str(page) + ".csv")
+    d_2020.to_csv(new_filename, sep="," , encoding='utf-8', index=False)
+
